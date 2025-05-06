420aade59381e0705075136b7507b5f51f476cb0
fatiperea <fatimaperea3pppp0@gmail.com>
2025-05-06 17:54:54 -0300

    creo la clase moneda

--- src/perea/conversor/modelos/Moneda.java
+++ src/perea/conversor/modelos/Moneda.java
@@ -1,4 +1,28 @@
 package perea.conversor.modelos;
 
 public class Moneda {
+
+    private String nombre;
+    private double tasaDeCambio;
+
+    public Moneda(String nombre, double tasaDeCambio) {
+        this.nombre = nombre;
+        this.tasaDeCambio = tasaDeCambio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getTasaDeCambio() {
+        return tasaDeCambio;
+    }
+
+    public void setTasaDeCambio(double tasaDeCambio) {
+        this.tasaDeCambio = tasaDeCambio;
+    }
 }
