517d7a479ea1e85c796d8f21b64ef44c02a52d77
fatiperea <fatimaperea3pppp0@gmail.com>
2025-05-10 18:16:19 -0300

    agrego excepcion y elimino comentarios

--- src/perea/conversor/principal/Principal.java
+++ src/perea/conversor/principal/Principal.java
@@ -5,41 +5,49 @@ import perea.conversor.modelos.TasaDeCambioER;
 
 import java.io.IOException;
 import java.net.URI;
-import java.net.URL;
+
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.Scanner;
 
 public class Principal {
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) {
+
         Scanner lectura= new Scanner(System.in);
         System.out.println("moneda: ");
         var busqueda=lectura.nextLine();
 
         String direccion="https://v6.exchangerate-api.com/v6/98940a4ca8e53fb94f327ebb/latest/"+busqueda;
 
-        URL url= new URL(direccion);
+        try {
 
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(direccion))
-                .build();
-        HttpResponse<String> response = client
-                .send(request, HttpResponse.BodyHandlers.ofString());
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(direccion))
+                    .build();
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
 
-        String json=response.body();
+            //String json=response.body();
+
+            JsonObject jsonObject = JsonParser.parseString(response.body()).getAsJsonObject();
+            double tasa= jsonObject.getAsJsonObject("conversion_rates").get("ARS").getAsDouble();
+
+            System.out.println("tasa: "+tasa);
+
+
+        } catch (Exception e) {
+            System.out.println("Error");
+            throw new RuntimeException(e);
+        }
 
         Gson gson= new Gson();
 
         //TasaDeCambioER tasa= gson.fromJson(json, TasaDeCambioER.class);
 
-        /*try {*/
 
-        JsonObject jsonObject = JsonParser.parseString(response.body()).getAsJsonObject();
-        double tasa= jsonObject.getAsJsonObject("conversion_rates").get(busqueda).getAsDouble();
 
-            System.out.println("tasa: "+tasa);
 
 
 
