a3db100920882cef7d859adc7c6f537c552bdd30
fatiperea <fatimaperea3pppp0@gmail.com>
2025-05-10 00:45:55 -0300

    se logra obtener tasa

--- src/perea/conversor/principal/Principal.java
+++ src/perea/conversor/principal/Principal.java
@@ -4,7 +4,10 @@ import com.google.gson.*;
 import perea.conversor.modelos.TasaDeCambioER;
 
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -17,6 +20,8 @@ public class Principal {
         var busqueda=lectura.nextLine();
 
         String direccion="https://v6.exchangerate-api.com/v6/98940a4ca8e53fb94f327ebb/latest/"+busqueda;
+
+        URL url= new URL(direccion);
 
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
@@ -32,17 +37,41 @@ public class Principal {
         //TasaDeCambioER tasa= gson.fromJson(json, TasaDeCambioER.class);
 
         //System.out.println(tasa);
+        /*try {*/
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
 
-        JsonElement elemento = JsonParser.parseString(response.body());
+            JsonObject jsonObject = JsonParser.parseReader(new InputStreamReader(conn.getInputStream())).getAsJsonObject();
+            double tasa=jsonObject.getAsJsonObject("conversion_rates").get(busqueda).getAsDouble();
+
+            //System.out.println("jo "+jsonObject);
+
+            System.out.println("tasa: "+tasa);
+
+        /*    if (conn.getResponseCode() != 200) {
+                System.out.println("Error al obtener datos de la API.");
+
+            }
+        }catch (Exception e){
+            {
+                e.printStackTrace();
+        }*/
+
+
+
+        /*JsonElement elemento = JsonParser.parseString(response.body());
         JsonObject objectRoot = elemento.getAsJsonObject();
         //Accediendo a JsonObject
-        double tasa = objectRoot.get("conversion_rate").getAsDouble();
+        double tasa = objectRoot.get("conversion_rate").getAsDouble();*/
 
-        System.out.println(objectRoot);
+        //System.out.println(objectRoot);
         //System.out.println("tasa: "+tasa);
+
+        /**/
 
 
 
 
     }
 }
+
