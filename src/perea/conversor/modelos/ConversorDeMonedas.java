0e3d2847de9da30974391bdd48acc5085e04655c
fatiperea <fatimaperea3pppp0@gmail.com>
2025-05-10 18:54:46 -0300

    aplico en la clase conversor lo q se habia pensado para tasa de cambio ya q no seria necesario tener varias clases para el proyecto

--- src/perea/conversor/modelos/ConversorDeMonedas.java
+++ src/perea/conversor/modelos/ConversorDeMonedas.java
@@ -1,17 +1,43 @@
 package perea.conversor.modelos;
 
 public class ConversorDeMonedas {
-    private Moneda monedaBase;
-    private Moneda monedaDestino;
 
-    public ConversorDeMonedas(Moneda monedaBase, Moneda monedaDestino) {
+    private String monedaBase;
+    private String monedaDestino;
+    private Double tasaCambio;
+
+    /*public ConversorDeMonedas(Moneda monedaBase, Moneda monedaDestino) {
         this.monedaBase = monedaBase;
         this.monedaDestino = monedaDestino;
+    }*/
+
+    public String getMonedaBase() {
+        return monedaBase;
+    }
+
+    public void setMonedaBase(String monedaBase) {
+        this.monedaBase = monedaBase;
+    }
+
+    public String getMonedaDestino() {
+        return monedaDestino;
+    }
+
+    public void setMonedaDestino(String monedaDestino) {
+        this.monedaDestino = monedaDestino;
+    }
+
+    public Double getTasaCambio() {
+        return tasaCambio;
+    }
+
+    public void setTasaCambio(Double tasaCambio) {
+        this.tasaCambio = tasaCambio;
     }
 
     public double convertir(double cantidad){
 
-        return (cantidad/monedaBase.getTasaDeCambio())*monedaDestino.getTasaDeCambio();
+        return cantidad * this.getTasaCambio();
 
     }
 }
--- src/perea/conversor/modelos/TasaDeCambio.java
+++ src/perea/conversor/modelos/TasaDeCambio.java
@@ -1,62 +1,21 @@
 package perea.conversor.modelos;
 
-import java.util.Map;
-
-/*
-    import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.Scanner;
-import org.json.JSONObject;
-
-public class ExchangeRateAPI {
-    private static final String API_URL = "https://v6.exchangerate-api.com/v6/TU_CLAVE_API/latest/";
-
-    public static double obtenerTasa(String monedaBase, String monedaDestino) {
-        try {
-            URL url = new URL(API_URL + monedaBase);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-
-            if (conn.getResponseCode() != 200) {
-                System.out.println("Error al obtener datos de la API.");
-                return -1;
-            }
-
-            Scanner scanner = new Scanner(conn.getInputStream());
-            String jsonResponse = "";
-            while (scanner.hasNext()) {
-                jsonResponse += scanner.nextLine();
-            }
-            scanner.close();
-
-            JSONObject jsonObject = new JSONObject(jsonResponse);
-            return jsonObject.getJSONObject("conversion_rates").getDouble(monedaDestino);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-            return -1;
-        }
-    }
-}
-
-    * */
 public class TasaDeCambio {
 
-    String resultado;
-    private Moneda monedaBase;
+    private String resultado;
+    private String monedaBase;
 
-    Map<String, Double> tasaCambio;
+    private Double tasaCambio;
 
     public String getResultado() {
         return resultado;
     }
 
-    public Moneda getMonedaBase() {
+    public String getMonedaBase() {
         return monedaBase;
     }
 
-    public Map<String, Double> getTasaCambio() {
+    public Double getTasaCambio() {
         return tasaCambio;
     }
 }
